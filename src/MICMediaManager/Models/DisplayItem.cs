7a2895dd02d18e6ec276924c760ae598538d773b
Joe Raio <joe.raio@microsoft.com>
2016-12-28 11:39:21 -0500

    Initial Commit

---
+++ src/MICMediaManager/Models/DisplayItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MICMediaManager.Models
+{
+    public class DisplayItem
+    {
+        public int Id { get; set; }
+        public int OrderIndex { get; set; }
+        public string ImageUri { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+
+    }
+}
