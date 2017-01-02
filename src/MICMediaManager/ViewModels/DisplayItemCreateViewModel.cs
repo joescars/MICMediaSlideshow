101d30cb289c18e814c85ae8286d8887a42dae16
Joe Raio <joe.raio@microsoft.com>
2017-01-02 16:11:47 -0500

    Misc cleanup

--- src/MICMediaManager/ViewModels/DisplayItemCreateViewModel.cs
+++ src/MICMediaManager/ViewModels/DisplayItemCreateViewModel.cs
@@ -9,11 +9,7 @@ namespace MICMediaManager.ViewModels
     public class DisplayItemCreateViewModel
     {
         public int Id { get; set; }
-        //public int OrderIndex { get; set; }
-        //public string ImageUri { get; set; }
         public bool IsActive { get; set; }
-        //public DateTime DateCreated { get; set; }
-        //public DateTime DateModified { get; set; }
         public IFormFile MediaFile { get; set; }
     }
 }
